46af64b02c2c6110c50296b434fa993a93b627f5
mroz <private@mrozo.pl>
2015-10-20 16:06:19 +0200

    moved testing code to a separate file

--- PyNAS.py
+++ PyNAS.py
@@ -1,32 +1,9 @@
-try:
-    from DevelopmentConfig import NasConf
-    print("Loaded DevelopementConf file")
-except ImportError:
-    from Config import NasConf
-    print("Loaded Conf file")
 
-from ConfigParser import config_parser_class_tests, ConfigParser
-from Partition import partition_class_tests
-from Disk import disk_class_tests
 
 __author__ = 'm'
 
 
 # todo .gitignore
 # todo learn a proper unit tests
-def py_nas_tests():
-    try:
-        config = ConfigParser(NasConf)
-    except Exception as E:
-        assert False, 'Failed to parse NasConfig\n' + str(E)
-
-    assert partition_class_tests(), 'Partition class tests have failed.'
-    assert disk_class_tests(), 'Disk class tests have failed.'
-    assert config_parser_class_tests(), 'Config parser tests have failed'
-
-    # todo parted tests
-    # todo hdparm tests
-
-py_nas_tests()
 
 # todo blkid wrapper
---
+++ Tests.py
@@ -0,0 +1,35 @@
+from ConfigParser import config_parser_class_tests, ConfigParser
+from Partition import partition_class_tests
+from Disk import disk_class_tests
+from Hardware import test_hardware_class
+try:
+    from DevelopmentConfig import NasConf
+
+    print("Loaded DevelopementConf file")
+except ImportError:
+    from Config import NasConf
+
+    print("Loaded Conf file")
+
+__author__ = 'm'
+
+
+def py_nas_tests():
+    try:
+        try:
+            config = ConfigParser(NasConf)
+        except Exception as E:
+            assert False, 'Failed to parse NasConfig\n' + str(E)
+
+        assert partition_class_tests(), 'Partition class tests have failed.'
+        assert disk_class_tests(), 'Disk class tests have failed.'
+        assert config_parser_class_tests(), 'Config parser tests have failed'
+        assert test_hardware_class(), "Hardware class test have failed"
+
+        # todo parted tests
+        # todo hdparm tests
+    except Exception as E:
+        print(E)
+
+
+py_nas_tests()
